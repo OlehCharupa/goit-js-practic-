0ecb3d36a006957f4768107c55c4fee6085a4553
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-03 15:36:28 +0300

    task-01-good

--- module-01.js
+++ module-01.js
@@ -1,7 +1,7 @@
 //  task-01
-let name = "Генератор защитного поля";
+const name = "Генератор защитного поля";
 let price = 1000;
 price = 2000;
 
-let inform = "Выбран " + name + ", цена за штуку " + price + " кредитов";
+const inform = `Выбран ${name}, цена за штуку ${price} кредитов.`;
 console.log(inform);
