1c37763c198c9e0d472476a7ddfaf42557dea7db
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-03 15:21:09 +0300

    task-01

---
+++ module-01.js
@@ -0,0 +1,7 @@
+//  task-01
+let name = "Генератор защитного поля";
+let price = 1000;
+price = 2000;
+
+let inform = "Выбран " + name + ", цена за штуку " + price + " кредитов";
+console.log(inform);
