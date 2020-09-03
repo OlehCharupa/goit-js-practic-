5c6eec71d60280786027a46554f46a1926157fb0
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-03 17:05:45 +0300

    практика

--- module-01.js
+++ module-01.js
@@ -1,7 +1,32 @@
-//  task-01
-const name = "Генератор защитного поля";
-let price = 1000;
-price = 2000;
+// //  task-01
+// const name = "Генератор защитного поля";
+// let price = 1000;
+// price = 2000;
 
-const inform = `Выбран ${name}, цена за штуку ${price} кредитов.`;
-console.log(inform);
+// const inform = `Выбран ${name}, цена за штуку ${price} кредитов.`;
+// console.log(inform);
+
+// const name = "Oleh";
+// let age = 27;
+// const messege = `Привет ${name}!!!`;
+// console.log(messege);
+
+// const newmessege = `My age is ${age}`;
+// console.log(newmessege);
+// age = 30;
+
+// const name = "oleh";
+// let age = prompt("Введите ваш возраст");
+// console.log(typeof age);
+// console.log(age);
+// // age = +age;
+// age = Number(age);
+// console.log(typeof age);
+
+let answer = confirm("Вам есть 18?");
+console.log(answer);
+console.log(typeof answer);
+
+// let messege = "hello" + name;
+// console.log(messege);
+// let newMessege = `my age is ${age}`;
