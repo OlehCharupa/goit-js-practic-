478e56d223008068570345d69cec95a0a82dd6d3
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2020-09-10 18:01:07 +0300

    ndfvujfv

--- 1009povtorenie.js
+++ 1009povtorenie.js
@@ -73,8 +73,7 @@
 //   }
 //   console.log(uniqueNumbers);
 // }
-
-addUniqueNumbers(1, 2, 3, 88, 97);
+// addUniqueNumbers(1, 2, 3, 88, 97);
 
 // //   uniqueNumbers.push(...args);
 // let newArr = [...uniqueNumbers];
@@ -82,3 +81,62 @@ addUniqueNumbers(1, 2, 3, 88, 97);
 //   newArt.push();
 // }
 // console.log(newArr);
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// ===================================================================
+// Напиши функцию logItems(array), которая получает массив и использует
+// цикл for, который для каждого элемента массива будет выводить в
+// консоль сообщение в формате[номер элемента]-[значение элемента].
+
+// Нумерация должна начинаться с 1. К примеру для первого элемента
+// массива['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено
+// '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
+
+//=====const logItems = function (array) {
+//=====  for (let i = 0; i < array.length; i++) {
+//=====    console.log(`${[i + 1]} - ${array[i]}`);
+//=====  }
+//=====};
+//=====
+//=====logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+//=====
+//=====logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+// ====================================================================
+// ====================================================================
+// ====================================================================
+// ====================================================================
+// ====================================================================
+// Напиши скрипт подсчета стоимости гравировки украшений.
+// Для этого создай функцию calculateEngravingPrice(message, pricePerWord)
+// принимающую строку(в строке будут только слова и пробелы)
+// и цену гравировки одного слова, и возвращающую цену гравировки
+// всех слов в строке.
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//   return message.split(" ").length * pricePerWord;
+// };
+const calculateEngravingPrice = (message, pricePerWord) => message.split(" ").length * pricePerWord;
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+); // 240
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+); // 120
